79102224d406a7db4d24de98cd1c424219b3cec0
ks <worldkks7@gmail.com>
2021-04-11 20:49:17 +0900

    섹션 4. 'API 개발 고급 - 컬렉션 조회 최적화' - V2

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,26 +1,49 @@
 package jpabook.jpashop.api;
 
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
-import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * V1. 엔티티 직접 노출
+ * - 엔티티가 변하면 API 스펙이 변한다.
+ * - 트랜잭션 안에서 지연 로딩 필요
+ * - 양방향 연관관계 문제 *
+
+ * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+ * - 트랜잭션 안에서 지연 로딩 필요
+ * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+ * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+ * V4.JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1+NQuery)
+ * - 페이징 가능
+ * V5.JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1+1Query)
+ * - 페이징 가능
+ * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
+ * - 페이징 불가능...
+ */
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
 
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리 * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
 
-        // 연관관계 강제 초기화
+        // 연관관계 엔티티 강제 초기화
         for (Order order : all) {
             order.getMember().getName();
             order.getDelivery().getAddress();
@@ -30,5 +53,57 @@ public class OrderApiController {
             orderItems.stream().forEach(o -> o.getItem().getName());
         }
         return all;
+    }
+
+    /**
+     * V2. N+1 문제 발생
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        // VO는 노출되도 상관 없다.
+        private Address address;
+        // Dto 내부에도 엔티티가 없어야 한다. 즉 response에 엔티티가 전혀 없어야한다. 그러므로 OrderItem도 dto로 변환해주어야 한다.
+        private List<OrderItemDto> orderItems;
+
+        // dto가 엔티티를 parameter로 받는 것은 문제가 되지 않는다. 별로 중요하지 않은 곳에서 중요한 엔티티를 받는 것이기 때문에.
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(OrderItemDto::new)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName; //상품 명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
