fcfb69177164a5be123b755a4d279b724f087a4d
ks <worldkks7@gmail.com>
2021-08-23 22:43:44 +0900

    섹션 5. 'API 개발 고급 - 실무 필수 최적화' - OSIV와 성능 최적화

---
+++ src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.service.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderQueryService {
+    private final OrderRepository orderRepository;
+
+    // 화면에 fit한 dto로 변환하는 쿼리 발생 로직을 다른 서비스로 분리 (커멘드와 쿼리 분리)(즉 화면애 관한 로직과 핵심 비즈니스 로직을 분리함.)
+    public List<OrderDto> ordersV2() {
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+
+        return result;
+    }
+}
